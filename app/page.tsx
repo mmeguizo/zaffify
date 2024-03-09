2c1eabb6303fafe1601fb4720078d9dcb08a3593
mmeguizo <mark.meguizo@gmail.com>
2024-03-09 12:35:49 +0800

    2nd commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <p className='text-3xl font-medium text-sky-700'>Hello bords </p>
+    <p className='text-3xl font-medium text-sky-700'>Hello bordss</p>
   )
 }
